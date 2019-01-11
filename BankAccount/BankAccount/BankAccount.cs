75a28749d2b45fd944d2c3d898e0f701441d9762
ginaisvor <ginaisvor@yahoo.com>
2019-01-11 20:39:39 +0200

    initial commit

---
+++ BankAccount/BankAccount/BankAccount.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        private string name;
+        private decimal sold;
+        private bool openedAccount;
+
+        public BankAccount()
+        {
+            this.openedAccount = true;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        
+        public decimal Sold
+        {
+            get
+            {
+                if (this.openedAccount)
+                {
+                    return this.sold;
+                }
+                else
+                {
+                    Console.WriteLine("This account is inexistent/inactive.");
+                    return 0;
+                }
+            }
+        }
+
+        public void Deposit(decimal suma)
+        {
+            if (this.openedAccount)
+            {
+                this.sold += suma;
+            }
+            else
+            {
+                Console.WriteLine("This account is inexistent/inactive.");
+            }
+        }
+
+        public void Withdraw(decimal suma)
+        {
+            if (this.sold >= suma && this.openedAccount)
+            {
+                 this.sold -= suma;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds or inexistent/inactive account.");
+            }
+        }
+
+        public void CloseAccount()
+        {
+            this.openedAccount = false;
+        }
+
+        public override string ToString()
+        {
+            return "In the account of mr/mrs " + this.Name + "has a sold of:  " + this.Sold + ". ";
+        }
+    }
+}
---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankAccount
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount cont = new BankAccount();
+            cont.Name = "Gina I.";
+            Console.WriteLine(cont.Sold);
+            cont.Deposit(2500);
+            Console.WriteLine(cont.Sold);
+            cont.Withdraw(2601);
+            cont.Withdraw(2450);
+            Console.WriteLine(cont.Sold);
+            cont.CloseAccount();
+            Console.WriteLine(cont.Sold);
+            cont.Deposit(5);
+        }
+    }
+}
+
